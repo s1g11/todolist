275db5c4cc0d2be2c2462d41ccd5612a3a921881
Roman Petrakovskiy <roman.petrakovskiy@gmail.com>
2022-07-03 01:26:27 +0700

    Updates

---
+++ precache-manifest.583fdfab18786fb5334ed64de3e1a4a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e714159df01dee9f59887200d71f9d4b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fccd9153af1e55b4b91b",
+    "url": "/todolist/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "7c10a9405fd26de1d386",
+    "url": "/todolist/static/js/2.9e141c36.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.9e141c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fccd9153af1e55b4b91b",
+    "url": "/todolist/static/js/main.352356b7.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
